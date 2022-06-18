124749175b1b4d1622ba9cb6a1d721ef6da778ef
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-06-18 14:00:57 -0300

    Apply spring session

--- ecommerce/src/main/resources/db/migration/V9.5__insert_product.sql
+++ ecommerce/src/main/resources/db/migration/V9.5__insert_product.sql
@@ -1,8 +1,2 @@
 insert into product (product_id, name, price, detail, image, is_available, stock, category_id, mark_id)
-values               (1,'Play station',50, 'detalles', 'imagen', true, 10, 1, 1),
-                     (2,'Nintendo', 4000, 'detalles', 'imagen', true, 10, 3, 3),
-                     (3,'Teclado mecanico T456', 10000, 'detalles', 'imagen', true, 10, 2, 2),
-                     (4,'Mouse GT567', 2000, 'detalles', 'imagen', true, 10, 1, 1),
-                     (5,'Silla de escritorio Smart Tech WST8021', 25000, 'detalles', 'imagen', true, 10, 4, 4),
-                     (6,'Kit de teclado y mouse inalámbrico', 2000, 'detalles', 'imagen', true, 10, 4, 4),
-                     (7,'Placa de video Nvidia', 52400, 'detalles', 'imagen', true, 10, 4, 4);
+values               (1,'Play station',50, 'detalles', 'imagen', true, 10, 1, 1);
